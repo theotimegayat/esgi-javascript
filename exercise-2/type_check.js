6ba20ff10a240bec97658cbced87f1b498717e85
Théotime Gayat <theotime.gayat@gmail.com>
2019-12-17 11:14:25 +0100

    test

--- exercise-2/type_check.js
+++ exercise-2/type_check.js
@@ -1,5 +1,17 @@
-function type_check_v1() {
+function type_check_v1(arg1, arg2) {
+    if(arg2 === "null") {
+        return arg1 === null ? true : false;
+    }
 
+    if(arg2 === "object") {
+        return arg1 === null ? false : true;
+    }
+
+    if(arg2 === "array") {
+        return Array.isArray(arg1) ? true : false
+    }
+
+    return typeof arg1 === arg2
 }
 
-console.log(type_check_v1());
+console.log(type_check_v1([], "array"))
